250b16246f74202f968112d2846eb66eda9fe78c
desairas <desairas@sheridancollege.ca>
2025-03-10 21:51:33 -0400

    feat: Trained YOLOv8 model with 25 epochs and saved as train8

--- ball_tracking_train.py
+++ ball_tracking_train.py
@@ -6,4 +6,4 @@ for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu,True)
 
 model = YOLO("yolov8s.pt")
-results = model.train(data="data.yaml", epochs=100)
+results = model.train(data="data.yaml", epochs=25)
